ad53954671ae560fac9c07f4b5d54362753c4920
Le Nhat Anh <lenhatanh86@gmail.com>
2016-06-28 05:27:59 +0700

    Update gallery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 $(document).ready(function () {
-    // $('.site_overlay').on('click', function(e) {
-    //     if($(e.target).hasClass('site_overlay')) {
-    //         $('.site_overlay').hide()
-    //         $('body').removeClass('has_overlay')
-    //         $('.modal_photo_item').html('<h2 class="photo_title"></h2><div class="photo_preview"></div>');
-    //     }
-    // })
-    //
-    // $('.photo').on('click', function(e) {
-    //     e.preventDefault()
-    //     $('body').addClass('has_overlay')
-    //     //$('.modal_photo_item .photo_title').text($(this).data('title'))
-    //     $('.modal_photo_item .photo_preview').empty().append($('<img/>').attr('src', $(this).data('large')))
-    //     $('.site_overlay').show()
-    // })
+    $('.modal_photo_item a.close').on('click', function(e) {
+        e.preventDefault()
+        $('.site_overlay').hide()
+        $('body').removeClass('has_overlay')
+        $('.modal_photo_item').css('background-image', '')
+    })
+
+    $('.photo').on('click', function(e) {
+        e.preventDefault()
+        $('body').addClass('has_overlay')
+        $('.modal_photo_item').css('background-image', 'url(' + $(this).data('large') + ')')
+        $('.site_overlay').show()
+    })
 })
